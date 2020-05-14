b050fe8ad3cf16a380ff3ae4f92fac46c3f3de5a
khalidh98-mrc <65328988+khalidh98-mrc@users.noreply.github.com>
2020-05-14 15:06:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'sampleFormat',         # How you named your package folder (MyLib)
   packages = ['sampleFormat'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Sample pip package format',   # Give a short description about your library
   author = 'KHALIDH AHAMED',                   # Type in your name
